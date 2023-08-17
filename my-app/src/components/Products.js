3a9a192f6b54b1cc1900b83634a470cf75c85406
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-17 20:59:03 +0500

    add cart through button

--- my-app/src/components/Products.js
+++ my-app/src/components/Products.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch , useSelector } from "react-redux";
 import {add} from '../store/cartSlice'
 const Products = () => {
   const dispatch  = useDispatch()
